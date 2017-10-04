eb4b7123e693461bd5a71e491096061f47a954c1
Jan-Erik Rediger <janerik@fnordig.de>
2017-10-04 14:57:21 -0300

    Add web frontend using rocket

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
 extern crate regex;
 extern crate tantivy;
 extern crate walkdir;
+extern crate rocket;
+extern crate rocket_contrib;
 
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::env;
 use std::time::Instant;
 
@@ -18,6 +25,10 @@ use tantivy::Index;
 use tantivy::schema::*;
 use tantivy::collector::{self, CountCollector, TopCollector};
 use tantivy::query::QueryParser;
+
+use rocket::State;
+use rocket::response::{Redirect, NamedFile};
+use rocket_contrib::Template;
 
 lazy_static! {
     static ref RE: Regex = Regex::new(r"(?x)
@@ -90,48 +101,78 @@ fn build_index(index_path: &Path, data_path: &str) -> Index {
     index
 }
 
+struct IndexServer {
+    index: Index,
+    query_parser: QueryParser,
+    schema: Schema,
+}
 
-fn main() {
-    let mut args = env::args().skip(1).peekable();
-    let data_path = "data";
+fn init_index() -> IndexServer {
+    println!("Loading index from path");
     let index_path = Path::new("idx");
-
-    let skip_idx = match args.peek() {
-        Some(a) if a == "skip" => true,
-        _ => false
-    };
-
-    let index = if skip_idx {
-        args.next();
-        println!("Loading index from path");
-        Index::open(index_path).expect("Can't load index")
-    } else {
-        build_index(&index_path, &data_path)
-    };
-
-    let has_limit = match args.peek() {
-        Some(a) if a == "limit" => true,
-        _ => false
-    };
-
-    let limit = if has_limit {
-        args.next();
-        args.next().unwrap().parse().unwrap_or(10)
-    } else {
-        10
-    };
+    let index = Index::open(index_path).expect("Can't load index");
 
     let schema = index.schema();
     let nick_field = schema.get_field("nick").unwrap();
     let msg_field = schema.get_field("msg").unwrap();
 
-    index.load_searchers().expect("Can't load searchers");
-    let searcher = index.searcher();
-
     let query_parser = QueryParser::new(index.schema(), vec![nick_field, msg_field]);
-    let user_query = args.collect::<Vec<_>>().join(" ");
-    println!("Searching: {}", user_query);
-    let query = query_parser.parse_query(&user_query).expect("Can't parse query");
+
+    IndexServer {
+        index: index,
+        query_parser: query_parser,
+        schema: schema
+    }
+}
+
+#[get("/")]
+fn index_site() -> Redirect {
+    Redirect::to("/search")
+}
+
+#[derive(FromForm)]
+struct Query {
+    q: Option<String>,
+    limit: Option<usize>,
+    _search: String,
+}
+
+#[derive(Serialize)]
+struct Result {
+    q: String,
+    num_hits: usize,
+    shown_hits: usize,
+    hits: Vec<Hit>,
+    limit_10: bool,
+    limit_50: bool,
+    limit_100: bool,
+}
+
+#[derive(Serialize)]
+struct Hit {
+    time: String,
+    nick: String,
+    msg: String,
+}
+
+#[get("/search")]
+fn search_site_no_query() -> Template {
+    Template::render("search", None::<()>)
+}
+
+#[get("/search?<query>")]
+fn search_site(idx: State<IndexServer>, query: Query) -> Template {
+    if query.q.is_none() {
+        return Template::render("search", None::<String>);
+    }
+
+    let user_query = query.q.unwrap();
+    let limit = query.limit.unwrap_or(10);
+
+    idx.index.load_searchers().expect("Can't load searchers");
+    let searcher = idx.index.searcher();
+
+    let query = idx.query_parser.parse_query(&user_query).expect("Can't parse query");
 
     let mut count_collector = CountCollector::default();
     let mut top_collector = TopCollector::with_limit(limit);
@@ -142,16 +183,56 @@ fn main() {
         searcher.search(&*query, &mut chained_collector).expect("Can't search");
     }
 
-    let doc_addresses = top_collector.score_docs();
-    println!("Showing {} relevant results ({} total):", doc_addresses.len(), count_collector.count());
-    for (score, doc_address) in doc_addresses {
-        let retrieved_doc = searcher.doc(&doc_address).expect("Can't get document");
-        let doc = schema.to_named_doc(&retrieved_doc);
-        let map = doc.0;
-        let time = &map["time"][0].text();
-        let nick = &map["nick"][0].text();
-        let msg = &map["msg"][0].text();
+    let doc_addresses = top_collector.docs();
 
-        println!("({:.2}) [{}] {:>12}> {}", score, time, nick, msg);
+    let hits = doc_addresses.into_iter().map(|da| {
+        let retrieved_doc = searcher.doc(&da).expect("Can't get document");
+        let doc = idx.schema.to_named_doc(&retrieved_doc);
+        let map = doc.0;
+
+        Hit {
+            time: map["time"][0].text().to_owned(),
+            nick: map["nick"][0].text().to_owned(),
+            msg:  map["msg"][0].text().to_owned(),
+        }
+    }).collect::<Vec<_>>();
+
+
+    let results = Result {
+        q: user_query,
+        num_hits: count_collector.count(),
+        shown_hits: hits.len(),
+        hits: hits,
+        limit_10: limit == 10,
+        limit_50: limit == 50,
+        limit_100: limit == 100,
+    };
+
+    return Template::render("search", results);
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+fn main() {
+    let mut args = env::args().skip(1).peekable();
+    let data_path = "data";
+    let index_path = Path::new("idx");
+
+
+    if let Some(a) =  args.next() {
+        if a == "index" {
+            build_index(&index_path, &data_path);
+            println!("Everything indexed.");
+            ::std::process::exit(0);
+        }
     }
+
+    rocket::ignite()
+        .mount("/", routes![index_site, search_site_no_query, search_site, files])
+        .attach(Template::fairing())
+        .manage(init_index())
+        .launch();
 }
