2b14d19f8fa74b5a5fc0783543886cfe5504ab18
Jan-Erik Rediger <janerik@fnordig.de>
2017-10-06 18:31:11 -0300

    Move indexing into its own module

---
+++ src/index.rs
@@ -0,0 +1,85 @@
+use std::path::Path;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::time::Instant;
+
+use tantivy::Index;
+use tantivy::schema::*;
+
+use walkdir::WalkDir;
+
+use regex::Regex;
+
+use errors::*;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"(?x)
+    (?P<time>\d{2}:\d{2})\s
+    [+@&]?
+    \s*
+    (?P<nick>[^\s][^>]+)
+    >
+    \s
+    (?P<msg>.+)").unwrap();
+
+    static ref WS: Regex = Regex::new(r"\s+").unwrap();
+}
+
+pub fn build_index(index_path: &Path, data_path: &str) -> Result<()> {
+    let mut schema_builder = SchemaBuilder::default();
+    schema_builder.add_text_field("time", TEXT | STORED);
+    schema_builder.add_text_field("nick", TEXT | STORED);
+    schema_builder.add_text_field("msg", TEXT | STORED);
+    let schema = schema_builder.build();
+
+    let index = Index::create(index_path, schema.clone())?;
+    let mut index_writer = index.writer(500_000_000)?;
+
+    let time_field = schema.get_field("time").unwrap();
+    let nick_field = schema.get_field("nick").unwrap();
+    let msg_field = schema.get_field("msg").unwrap();
+
+    let mut count = 0;
+    println!("Indexing...");
+
+    let now = Instant::now();
+    for entry in WalkDir::new(data_path) {
+        let entry = entry.unwrap();
+        if entry.file_type().is_dir() { continue; }
+        let date = entry.path().file_stem().expect("Can't stem filename");
+        let date = date.to_string_lossy();
+
+        let file = File::open(entry.path())?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line?;
+            let caps = match RE.captures(&line) {
+                Some(m) => m,
+                None => continue
+            };
+
+            let datetime = format!("{} {}", date, &caps["time"]);
+
+            if WS.is_match(&caps["nick"]) { continue; }
+
+            let mut doc = Document::default();
+            doc.add_text(time_field.clone(), &datetime);
+            doc.add_text(nick_field.clone(), &caps["nick"]);
+            doc.add_text(msg_field.clone(),  &caps["msg"]);
+            index_writer.add_document(doc);
+
+            count += 1;
+        }
+    }
+    println!("Indexing took {} seconds", now.elapsed().as_secs());
+    let now = Instant::now();
+    index_writer.commit().expect("Can't write index");
+    println!("Writing index took {} seconds", now.elapsed().as_secs());
+
+    println!("Indexed {} lines", count);
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,7 @@ extern crate structopt;
 #[macro_use] extern crate structopt_derive;
 #[macro_use] extern crate error_chain;
 
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
 use std::path::{Path, PathBuf};
-use std::time::Instant;
-
-use regex::Regex;
-use walkdir::WalkDir;
 
 use tantivy::Index;
 use tantivy::schema::*;
@@ -35,7 +28,10 @@ use rocket_contrib::Template;
 use structopt::StructOpt;
 
 mod errors;
+mod index;
+
 use errors::*;
+use index::build_index;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "irc-index")]
@@ -57,76 +53,6 @@ enum Opt {
     }
 }
 
-lazy_static! {
-    static ref RE: Regex = Regex::new(r"(?x)
-    (?P<time>\d{2}:\d{2})\s
-    [+@&]?
-    \s*
-    (?P<nick>[^\s][^>]+)
-    >
-    \s
-    (?P<msg>.+)
-                                      ").unwrap();
-
-    static ref WS: Regex = Regex::new(r"\s+").unwrap();
-}
-
-fn build_index(index_path: &Path, data_path: &str) -> Result<()> {
-    let mut schema_builder = SchemaBuilder::default();
-    schema_builder.add_text_field("time", TEXT | STORED);
-    schema_builder.add_text_field("nick", TEXT | STORED);
-    schema_builder.add_text_field("msg", TEXT | STORED);
-    let schema = schema_builder.build();
-
-    let index = Index::create(index_path, schema.clone())?;
-    let mut index_writer = index.writer(500_000_000)?;
-
-    let time_field = schema.get_field("time").unwrap();
-    let nick_field = schema.get_field("nick").unwrap();
-    let msg_field = schema.get_field("msg").unwrap();
-
-    let mut count = 0;
-    println!("Indexing...");
-
-    let now = Instant::now();
-    for entry in WalkDir::new(data_path) {
-        let entry = entry.unwrap();
-        if entry.file_type().is_dir() { continue; }
-        let date = entry.path().file_stem().expect("Can't stem filename");
-        let date = date.to_string_lossy();
-
-        let file = File::open(entry.path())?;
-        let reader = BufReader::new(file);
-
-        for line in reader.lines() {
-            let line = line?;
-            let caps = match RE.captures(&line) {
-                Some(m) => m,
-                None => continue
-            };
-
-            let datetime = format!("{} {}", date, &caps["time"]);
-
-            if WS.is_match(&caps["nick"]) { continue; }
-
-            let mut doc = Document::default();
-            doc.add_text(time_field.clone(), &datetime);
-            doc.add_text(nick_field.clone(), &caps["nick"]);
-            doc.add_text(msg_field.clone(),  &caps["msg"]);
-            index_writer.add_document(doc);
-
-            count += 1;
-        }
-    }
-    println!("Indexing took {} seconds", now.elapsed().as_secs());
-    let now = Instant::now();
-    index_writer.commit().expect("Can't write index");
-    println!("Writing index took {} seconds", now.elapsed().as_secs());
-
-    println!("Indexed {} lines", count);
-
-    Ok(())
-}
 
 struct IndexServer {
     index: Index,
